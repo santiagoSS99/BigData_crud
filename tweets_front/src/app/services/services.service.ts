52ff3cd427b67cc6e03bdb01f2a462fe8f1c34ed
santiagoSS99 <santiagoxq@icloud.com>
2023-04-28 18:14:19 -0500

    services to consume endpoints

--- tweets_front/src/app/services/services.service.ts
+++ tweets_front/src/app/services/services.service.ts
@@ -2,6 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Usuario } from '../interfaces/usuario';
 import { Observable } from 'rxjs';
+import { Tweet } from '../interfaces/tweet';
+import { Mencion } from '../interfaces/mencion';
+import { Hashtag } from '../interfaces/hasthag';
 
 @Injectable({
   providedIn: 'root'
@@ -16,11 +19,16 @@ export class ServicesService {
     return this.http.post<Usuario>(`${this.BASE_URL}/usuario/crear`, usuario)
   }
   // Cambiar
-  crearTweet(usuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>(`${this.BASE_URL}/usuario/crear`, usuario)
+  crearTweet(userId: string): Observable<Tweet> {
+    return this.http.post<Tweet>(`${this.BASE_URL}/tweets/create${userId}`, null, { responseType: 'json' })
   }
   // Cambiar
-  crearMencion(usuario: Usuario): Observable<Usuario> {
-    return this.http.post<Usuario>(`${this.BASE_URL}/usuario/crear`, usuario)
+  crearMencion(nick: string, mencion: Mencion): Observable<Usuario> {
+    return this.http.post<Usuario>(`${this.BASE_URL}/mencion/${nick}`, mencion)
   }
+
+  crearHashtag(hashtag: Hashtag, etiqueta: string): Observable<Hashtag> {
+    return this.http.post<Hashtag>(`${this.BASE_URL}/hashtag/crear/${etiqueta}`, hashtag)
+  }
+
 }
